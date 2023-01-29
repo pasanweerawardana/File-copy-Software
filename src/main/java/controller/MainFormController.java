0357a65c2827d51d7d7ed6b39b09dfdc2ff826b8
pasanweerawardana <pasanm16gmail.com>
2023-01-29 15:55:06 +0530

    finishing the functionality update

--- src/main/java/controller/MainFormController.java
+++ src/main/java/controller/MainFormController.java
@@ -1,7 +1,12 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
@@ -9,10 +14,8 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.text.NumberFormat;
 import java.util.Optional;
 
 public class MainFormController {
@@ -41,7 +44,7 @@ public class MainFormController {
 
         srcFile = fileChooser.showOpenDialog(lblFolder.getScene().getWindow());
         if (srcFile != null) {
-            lblFile.setText(srcFile.getName() + ". " + (srcFile.length() / 1024.0) + "Kb");
+            lblFile.setText(srcFile.getName() + ". " + formatNumber(srcFile.length() / 1024.0) + " Kb");
         } else {
             lblFile.setText("No file selected");
         }
@@ -65,7 +68,6 @@ public class MainFormController {
     }
 
     public void btnCopyOnAction(ActionEvent actionEvent) throws IOException {
-        FileInputStream fis = new FileInputStream(srcFile);
 
         File destFile = new File(destDir, srcFile.getName());
         if (!destFile.exists()) {
@@ -79,23 +81,71 @@ public class MainFormController {
             }
         }
 
-        FileOutputStream fos = new FileOutputStream(destFile);
+        /* Task class is a java fx specific thing, we can't use anywhere else */
+        /* 1. First we implement Task abstract class -> Anonymous Inner Class */
+        /* 2. Then we create an object from the newly implemented class */
+        /* 3. Lastly we store the object memory location */
+        var task = new Task<Void>() /* class Anonymous extends Task<Void>*/ {    // <- Don't worry about this wired syntax yet
+            @Override
+            protected Void call() throws Exception {
+                FileInputStream fis = new FileInputStream(srcFile);
+                FileOutputStream fos = new FileOutputStream(destFile);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-        int[] byteBuffer = new int[(int) srcFile.length()];
-        for (int i = 0; i < byteBuffer.length; i++) {
-            byteBuffer[i] = fis.read();
-            fos.write(byteBuffer[i]);
-        }
+                long fileSize = srcFile.length();
+                int totalRead = 0;
+                while(true) {
+                    byte[] buffer = new byte[1024 * 10];        // 10 Kb
+                    int read = bis.read(buffer);
+                    totalRead += read;
+                    if (read == -1) break;
+                    bos.write(buffer, 0, read);
+                    updateProgress(totalRead, fileSize);
+                }
 
-        fos.close();
-        fis.close();
+                updateProgress(fileSize, fileSize);
 
-        new Alert(Alert.AlertType.INFORMATION, "File has been copied successfully").show();
-        srcFile = null;
-        destDir = null;
-        lblFolder.setText("No folder selected");
-        lblFile.setText("No file selected");
-        btnCopy.setDisable(true);
+                bos.close();
+                bis.close();
 
+                return null;
+            }
+        };
+
+        task.workDoneProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number prevWork, Number curWork) {
+                pgbBar.setWidth(pgbContainer.getWidth() / task.getTotalWork() * curWork.doubleValue());
+                lblProgress.setText("Progress: " + formatNumber(task.getProgress() * 100) + "%");
+                lblSize.setText(formatNumber(task.getWorkDone() / 1024.0) + " / " + formatNumber(task.getTotalWork() / 1024.0) + " Kb");
+            }
+        });
+
+        task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent workerStateEvent) {
+                pgbBar.setWidth(pgbContainer.getWidth());
+                new Alert(Alert.AlertType.INFORMATION, "File has been copied successfully").showAndWait();
+                lblFolder.setText("No folder selected");
+                lblFile.setText("No file selected");
+                btnCopy.setDisable(true);
+                pgbBar.setWidth(0);
+                lblProgress.setText("Progress: 0%");
+                lblSize.setText("0 / 0 Kb");
+                srcFile = null;
+                destDir = null;
+            }
+        });
+
+        new Thread(task).start();
+    }
+
+    private String formatNumber(double input) {
+        NumberFormat ni = NumberFormat.getNumberInstance();
+        ni.setGroupingUsed(true);
+        ni.setMinimumFractionDigits(2);
+        ni.setMaximumFractionDigits(2);
+        return ni.format(input);
     }
 }
